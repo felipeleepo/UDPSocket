8c2288505b650c398180103b77d025f53f429613
Felipe Barbosa <felipebinfo@gmail.com>
2018-09-27 00:21:27 -0300

    Commit

---
+++ Cliente/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+            ProtocolType.Udp);
+
+            bool repita = true;
+            String x;
+            IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+            do
+            {
+                Console.WriteLine("Digite a informação:");
+                x = Console.ReadLine();
+                byte[] sendbuf = Encoding.ASCII.GetBytes(x);
+                IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+                s.SendTo(sendbuf, ep);
+                if (x != "FIM")
+                    Console.WriteLine("Mensagem enviada para o endereço de broadcast");
+                else
+                    repita = false;
+            } while (repita);
+
+            Console.WriteLine("Saindo");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            UdpClient listener = new UdpClient(11000);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 11000);
+
+            try
+            {
+                int cont = 0;
+                Console.WriteLine("Esperando transmissão:");
+                String content = "";
+                while (!done)
+                {
+
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    if (cont == 0)
+                        Console.WriteLine("Recebendo de {0} :", groupEP.ToString());
+                    cont++;
+
+                    content = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+
+                    if (content != "FIM")
+                        Console.WriteLine(content);
+                    else
+                        done = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Transmissão finalizada.");
+                listener.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
